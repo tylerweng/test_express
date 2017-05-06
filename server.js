783b25933b2873ca34f655031f2f43712fbb8654
Tyler Weng <tylercweng@gmail.com>
2017-05-06 12:16:57 -0700

    Update functionality

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var app = express();
+var fs = require("fs");
+
+var bodyParser = require('body-parser');
+var multer  = require('multer');
+var upload = multer({ dest: './uploads' });
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(multer({ dest: '/tmp/'}));
+
+// Create route for /index.html
 app.get('/index.html', function(req, res) {
   res.sendFile( __dirname + "/" + "index.html" );
+});
+
+// Create route for /upload.html
+app.get('/upload.html', function(req, res) {
+  res.sendFile( __dirname + "/" + "upload.html" );
 });
 
 app.get('/process_get', function(req, res) {
@@ -14,6 +28,28 @@ app.get('/process_get', function(req, res) {
   };
   console.log(response);
   res.end(JSON.stringify(response));
+});
+
+app.post('/file_upload', function(req, res) {
+  console.log(req.files.file.name);
+  console.log(req.files.file.path);
+  console.log(req.files.file.type);
+  var file = __dirname + "/" + req.files.file.name;
+
+  fs.readFile(req.files.file.path, function(err, data) {
+    fs.writeFile(file, data, function(err) {
+      if(err) {
+        console.log(err);
+      } else {
+        response = {
+          message: 'File uploaded successfully',
+          filename: req.files.file.name
+        };
+      }
+      console.log(response);
+      res.end(JSON.stringify(response));
+    });
+  });
 });
 
 // This responds with "Hello World" on the homepage
